5bd99442f575d3a026859e81534cd849e6f98ad9
Tausif Iqbal <tausifiqbalti21@gmail.com>
2023-10-09 18:55:42 +0530

    updated workflow

---
+++ docs/metrics/documentation/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sequencing_5fmetrics_5ftools_0',['Sequencing_Metrics_Tools',['../md_README.html',1,'']]]
+];
---
+++ docs/metrics/documentation/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sequencing_5fmetrics_5ftools_1',['Sequencing_Metrics_Tools',['../md_README.html',1,'']]]
+];
