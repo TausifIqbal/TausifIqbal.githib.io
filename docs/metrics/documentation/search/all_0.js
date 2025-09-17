51312571b8d1b11dcf2901871f044a68d8c4cbd9
Tausif Iqbal <tausifiqbalti21@gmail.com>
2025-09-17 22:30:19 +0530

    Add index page

--- docs/metrics/documentation/search/all_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sequencing_5fmetrics_5ftools_0',['Sequencing_Metrics_Tools',['../md_README.html',1,'']]]
-];
--- docs/metrics/documentation/search/pages_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['sequencing_5fmetrics_5ftools_1',['Sequencing_Metrics_Tools',['../md_README.html',1,'']]]
-];
